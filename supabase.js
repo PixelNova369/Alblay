9a46d9ca3d92096f076d2190c994ab265044a168
PixelNova369 <157920778+PixelNova369@users.noreply.github.com>
2026-06-15 18:24:49 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,16 +4,12 @@ const supabaseUrl = "https://imsevturnvlegnmszuyx.supabase.co/rest/v1/"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imltc2V2dHVybnZsZWdubXN6dXl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MjA5NTEsImV4cCI6MjA5NzA5Njk1MX0.1auc5wncmyL7OukXaP13lmuhl_PuPCAIXAqADnztiGg"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
 export async function saveAlbum() {
-  const { data, error } = await supabase
+  const result = await supabase
     .from('albums')
     .insert([
-      {
-        title: "Test Album"
-      }
+      { title: "Test Album" }
     ])
 
-  console.log("DATA:", data)
-  console.log("ERROR:", error)
+  alert(JSON.stringify(result, null, 2))
 }
