fa519fd26dfe833bca395bdefb375bd988226975
PixelNova369 <157920778+PixelNova369@users.noreply.github.com>
2026-06-15 19:29:10 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = "https://imsevturnvlegnmszuyx.supabase.co"
-"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imltc2V2dHVybnZsZWdubXN6dXl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MjA5NTEsImV4cCI6MjA5NzA5Njk1MX0.1auc5wncmyL7OukXaP13lmuhl_PuPCAIXAqADnztiGg"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
@@ -39,7 +38,7 @@ export async function saveAlbum(album) {
   ])
 }
 
-// LOAD ALBUMS (album wall)
+// LOAD ALBUMS
 export async function loadAlbums() {
   const { data } = await supabase
     .from('albums')
