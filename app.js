01fd5f4247544b7f851427323fd5ab50faeee2f1
PixelNova369 <157920778+PixelNova369@users.noreply.github.com>
2026-06-17 15:58:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("JS IS RUNNING")
+console.log("app.js is loading")
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM ready")
+
+  const testBtn = document.getElementById("testBtn")
+
+  if (testBtn) {
+    testBtn.addEventListener("click", () => {
+      alert("BUTTON WORKS")
+    })
+  }
+})
