aedfe4a34b6c025e52996771fedc46d6df31390a
PixelNova369 <157920778+PixelNova369@users.noreply.github.com>
2026-06-17 14:05:40 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,52 +1,36 @@
 import { supabase } from './supabase.js'
-alert("auth.js loaded")
+
 window.addEventListener("DOMContentLoaded", () => {
 
   const loginBtn = document.getElementById("loginBtn")
   const signupBtn = document.getElementById("signupBtn")
 
-  // =====================
-  // LOGIN
-  // =====================
-  if (loginBtn) {
-    loginBtn.addEventListener("click", async () => {
+  loginBtn.onclick = async () => {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
 
-      const email = document.getElementById("email").value
-      const password = document.getElementById("password").value
-
-      const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password
-      })
-
-      if (error) {
-        alert(error.message)
-      } else {
-        window.location.href = "app.html"
-      }
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password
     })
+
+    if (error) return alert(error.message)
+
+    window.location.href = "app.html"
   }
 
-  // =====================
-  // SIGNUP
-  // =====================
-  if (signupBtn) {
-    signupBtn.addEventListener("click", async () => {
+  signupBtn.onclick = async () => {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
 
-      const email = document.getElementById("email").value
-      const password = document.getElementById("password").value
-
-      const { error } = await supabase.auth.signUp({
-        email,
-        password
-      })
-
-      if (error) {
-        alert(error.message)
-      } else {
-        alert("Account created. Now log in.")
-      }
+    const { error } = await supabase.auth.signUp({
+      email,
+      password
     })
+
+    if (error) return alert(error.message)
+
+    alert("Account created")
   }
 
 })
